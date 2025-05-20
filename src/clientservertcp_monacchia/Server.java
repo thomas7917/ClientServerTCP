d7f053ca68ab6867809e545a54f618a40ba5da69
thomas7917 <r.monaccuia67@gmail.com>
2025-05-20 10:51:17 +0200

    Aggiornamento Server.java

--- src/clientservertcp_monacchia/Server.java
+++ src/clientservertcp_monacchia/Server.java
@@ -9,13 +9,61 @@ package clientservertcp_monacchia;
  *
  * @author studente
  */
-public class MainClient {
-    public static void main(String[] args) {
-        Client client = new Client("Mario", "rosso");
-        if (client.connetti("localhost", 12345) == 0) {
-            client.scrivi("Ciao dal client!");
-            client.leggi();
-            client.chiudi();
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private int porta;
+
+    public Server(int porta) {
+        this.porta = porta;
+    }
+
+    public void attendi() {
+        try {
+            serverSocket = new ServerSocket(porta);
+            System.out.println("Server in ascolto sulla porta " + porta);
+            clientSocket = serverSocket.accept();
+            System.out.println("Client connesso.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void scrivi(String message) {
+        try {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            out.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void leggi() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String input = in.readLine();
+            System.out.println("Ricevuto dal client: " + input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void chiudi() {
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void termina() {
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
