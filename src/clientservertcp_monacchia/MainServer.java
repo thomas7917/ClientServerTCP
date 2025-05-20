d6329baad60471d676f3effc4015ac04f6a2f402
thomas7917 <r.monaccuia67@gmail.com>
2025-05-20 10:50:04 +0200

    Aggiornamento MainServer.java

--- src/clientservertcp_monacchia/MainServer.java
+++ src/clientservertcp_monacchia/MainServer.java
@@ -10,5 +10,12 @@ package clientservertcp_monacchia;
  * @author studente
  */
 public class MainServer {
-    
+    public static void main(String[] args) {
+        Server server = new Server(12345);
+        server.attendi();
+        server.leggi();
+        server.scrivi("Ciao dal server!");
+        server.chiudi();
+        server.termina();
+    }
 }
