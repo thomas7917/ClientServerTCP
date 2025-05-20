54f642f41a99a461ef5569c97fdd1714db70123f
thomas7917 <r.monaccuia67@gmail.com>
2025-05-20 10:50:39 +0200

    AggiornamentoServer.java

--- src/clientservertcp_monacchia/Server.java
+++ src/clientservertcp_monacchia/Server.java
@@ -9,6 +9,13 @@ package clientservertcp_monacchia;
  *
  * @author studente
  */
-public class Server {
-    
+public class MainClient {
+    public static void main(String[] args) {
+        Client client = new Client("Mario", "rosso");
+        if (client.connetti("localhost", 12345) == 0) {
+            client.scrivi("Ciao dal client!");
+            client.leggi();
+            client.chiudi();
+        }
+    }
 }
