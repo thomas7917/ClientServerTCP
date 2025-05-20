bb1a5faa42c6af762b9d3b05abe20bcb8c31e80a
thomas7917 <r.monaccuia67@gmail.com>
2025-05-20 10:49:16 +0200

    Aggiornamento Client.java

--- src/clientservertcp_monacchia/Client.java
+++ src/clientservertcp_monacchia/Client.java
@@ -9,6 +9,63 @@ package clientservertcp_monacchia;
  *
  * @author studente
  */
+
+/**
+ *
+ * @author Monacchia
+ */
+import java.io.*;
+import java.net.*;
+
 public class Client {
-    
+    private String name;
+    private String color;
+    private Socket socket;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public Client(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public int connetti(String nomeServer, int portaServer) {
+        try {
+            socket = new Socket(nomeServer, portaServer);
+            return 0; // successo
+        } catch (IOException e) {
+            e.printStackTrace();
+            return -1; // errore
+        }
+    }
+
+    public void scrivi(String message) {
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void leggi() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String risposta = in.readLine();
+            System.out.println("Ricevuto dal server: " + risposta);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void chiudi() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
