15c5b5924685377146101fffda5ae0af1757226b
thomas7917 <r.monaccuia67@gmail.com>
2025-05-20 10:52:21 +0200

    Aggiornamento ClientServerTCP_Monacchia.java

--- src/clientservertcp_monacchia/ClientServerTCP_Monacchia.java
+++ src/clientservertcp_monacchia/ClientServerTCP_Monacchia.java
@@ -9,12 +9,12 @@ package clientservertcp_monacchia;
  * @author studente
  */
 public class ClientServerTCP_Monacchia {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Client client = new Client("Mario", "rosso");
+        if (client.connetti("localhost", 12345) == 0) {
+            client.scrivi("Ciao dal client!");
+            client.leggi();
+            client.chiudi();
+        }
     }
-    
 }
